2fb70d1add4fa4b9cea94676c96813cd3ee9c453
Érika Assis <erika.s.assis@gmail.com>
2019-08-06 23:37:07 -0300

    Adicionado back-end

---
+++ backend/src/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const DevController = require('./controller/DevController');
+const LikeController = require('./controller/LikeController');
+const DislikeController = require('./controller/DislikeController');
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+    return res.send('Ola World');
+});
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
+
+module.exports = routes;
